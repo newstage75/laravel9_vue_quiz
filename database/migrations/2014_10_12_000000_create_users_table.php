48b1fc00f3b7dfe989d143ed8db52f7ef0f103f9
github newstage75 <rightkingdxanntume@gmail.com>
2022-02-28 08:44:54 +0900

    記事テーブルとユーザーテーブルの作成
    外部キー制約として、
    _create_articles_table.phpを作成し、一部編集を加えた。
    
    また、親のテーブルであるusersテーブルのname,passwordをuniqueとした。

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique(); //20220228ユニークへ変更
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->unique(); //20220228ユニークへ変更
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
