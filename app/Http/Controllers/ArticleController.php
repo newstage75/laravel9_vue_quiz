ff80c954be022a169610ac85c639f55ecf7bbad0
github newstage75 <rightkingdxanntume@gmail.com>
2022-02-28 21:36:38 +0900

    ArticleRequestの実装
    store(記事保存のPOST)メソッドをコントローラに追加
    
    storeアクションメソッドは、第一引数が$requestとなっています。
    その$requestの手前にはArticleRequestと記述されています。
    これは、引数$requestはArticleRequestクラスのインスタンスである、ということを宣言しています。
    宣言することでどうなるかというと、もしstoreメソッドの第一引数に、ArticleRequestクラスのインスタンス以外のものが渡されるとTypeErrorという例外が発生して処理は中断します。

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Http\Requests\ArticleRequest;
 
 use Illuminate\Http\Request;
 
@@ -21,6 +22,15 @@ public function index()
   public function create()
   {
       return view('articles.create');
+  }
+
+  public function store(ArticleRequest $request, Article $article)
+  {
+      $article->title = $request->title;
+      $article->body = $request->body;
+      $article->user_id = $request->user()->id;
+      $article->save();
+      return redirect()->route('articles.index');
   }
 
 }
